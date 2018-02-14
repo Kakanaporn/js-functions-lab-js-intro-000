16c6ed8e00f2538f063c09df515b6c77b7f15c94
skakanaporn@gmail.com <skakanaporn@gmail.com>
2018-02-14 15:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function happyHolidays() {
+function happyHolidays(){
   return "Happy holidays!";
 }
 
@@ -7,10 +7,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo(้holiday, name){
+function happyHolidaysTo(holiday, name){
   return  `${holiday}, ${name}!`;
 }
 
-function holidaysCountdown(้holiday, day){
+function holidaysCountdown(holiday, day){
   return `It\'s ${days} days until ${holiday}!`;
 }
